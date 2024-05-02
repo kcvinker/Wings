73122ee4735f6aa823e09fc3a4bb4c72bb2fe94f
kcvinu <kcvinu82@gmail.com>
2024-05-03 02:27:17 +0530

    Removed unwanted files

--- TCR_Burner.d
+++
@@ -1,9 +0,0 @@
-module TCR_Burner;
-
-import wings;
-
-void main() {
-	auto frm = new Window("Time code Burner", 500, 400);
-	frm.create();
-	frm.show();
-}
--- make123.py
+++
@@ -1,3 +0,0 @@
-incpath = r"C:\Users\kcvin\OneDrive\Programming\D_Lang\Wings"           # IMPORTANT VAR
-dbgcmd = ["dmd", f"-I=\"{incpath}\"", "-m64", "-debug", "-i", "-run"]   # IMPORTANT VAR
-rlscmd = ["dmd", f"-I=\"{incpath}\"", "-m64", "-release", "-i"]         # IMPORTANT VAR
